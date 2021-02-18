123c247138df0503ce4bc92b5dc9f57b47fb6b3f
handsomeaddress <yeiehahaha@gmail.com>
2021-02-18 21:27:37 +0300

    init: Towel Sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	let result = '';
+	if(matrix){
+		matrix.forEach((el,i) => {
+			i++;
+			if(i % 2 == 0){
+				result += el.reverse().toString();
+			}else{
+				result += el.toString();
+			}
+			result += ',';
+		});
+	}
+	result = result.split(',');
+	return result.splice(0, result.length - 1);
 }
